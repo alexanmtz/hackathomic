934e69f44c89981c4a8d50c1e6b070c3fcceb1b2
Alexandre Magno Teles Zimerer <alexanmtz@Alexandres-MacBook-Pro.local>
2015-11-08 02:06:52 -0200

    the user authentication with devise

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem "font-awesome-rails"
 
+gem 'devise', '~> 3.4.1'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
+  
+  root to: "hackathomic#index"
+  
   get 'hackathomic/styleguide'
 
   get 'hackathomic/index'
+  
+  devise_for :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
